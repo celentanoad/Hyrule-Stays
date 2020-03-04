65a9c0ce551659f59211bccb66428ba09ad17b02
Alanna Celentano <alannacelentano@Alannas-MacBook-Pro.local>
2020-03-03 18:31:45 -0600

    update delete reviews function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,5 @@
 const Stay = require("../models/stay");
+const User = require("../models/user");
 
 module.exports = {
     create,
@@ -17,13 +18,22 @@ function create(req, res) {
     });
 }
 
-function deleteOne(req,res) {
-    if (stay.userCreated.equals(req.user._id)) {
-    Stay.findById(req.params.id, function(err, stay) {
-        console.log(stay.review._id);
-        res.redirect(`/stays/${req.params.id}`);
-        })
-    } else {
-        res.redirect(`/stays/${req.params.id}`);
-    }
+
+function deleteOne(req, res) {
+    
+    Stay.findById(req.params.stay_id, function(err, stay) {
+        console.log(stay);
+        stay.reviews.splice(req.params.idx, 1);
+    })
+    
 }
+//     console.log(req.params.id);
+//     if (review.userId.equals(req.user._id)) {
+//     Stay.findById(req.params.id, function(err, stay) {
+//         console.log(stay.review._id);
+//         res.redirect(`/stays/${req.params.id}`);
+//         })
+//     } else {
+//         res.redirect(`/stays/${req.params.id}`);
+//     }
+// }
