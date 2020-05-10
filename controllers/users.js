b6500df228835b19dec2e915658ad5cae16408fd
Alanna Celentano <alannacelentano@alannas-mbp.attlocal.net>
2020-05-10 10:14:27 -0500

    clean up code

--- controllers/users.js
+++ controllers/users.js
@@ -26,29 +26,5 @@ function createAlias(req, res) {
 }
 
 function addFavorite(req, res) {
-    console.log(req.query.name)
     res.redirect("/stays");
 }
-//     let stay = Stay.findById()
-
-//     Stay.findById(req.params._id, function(err, stay) {
-//         console.log(stay);
-//         User.findById(req.user._id, function(err, user) {
-//             // user.favorites.push(stay);
-//             user.save(function(err) {
-//                 res.redirect(`/users/${req.user}`);
-//             })
-//         })
-//     })
-// }
-//     stay = Stay.findById(req.params._id);
-//     console.log(stay)
-//     User.findById(req.user._id, function(err, user) {
-//         // user.favorites.push(stay);
-//         user.save(function(err) {
-//             res.redirect(`/users/${req.user}`);
-//         });
-//         console.log(user.name)
-//         console.log(user.favorites)
-//     });
-// }
